4decb789cd7a8f45b579579accc405cb1f5ba58f
Charles Leifer <coleifer@gmail.com>
2026-01-15 07:27:48 -0600

    Update setup.py to support python2.7.
    Refs #208

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import find_packages
 from setuptools import setup
 
+import os
+import re
+# 2.7 shim.
+with open(os.path.join(os.path.dirname(__file__),
+                       'walrus/__init__.py'), 'rt') as fh:
+    version, = [l for l in fh.readlines() if l.startswith('__version__ = ')]
+    version = re.search(r'\'(\d+\.\d+\.\d+)\'', version).groups()[0]
+
 setup(name='walrus',
+      version=version,
       install_requires=['redis>=3.0.0'],
       packages=find_packages(),
       package_data={'walrus': ['scripts/*', 'stopwords.txt']})
